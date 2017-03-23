29e267e9d5c9716f5e4ab7039f448bd0876014c2
aaronrainbird <aaronrainbird@gmail.com>
2017-03-23 16:06:38 +0000

    Update inject-video.js

--- inject-video.js
+++ inject-video.js
@@ -226,7 +226,7 @@ function defineChoices() {
     
         findPid();
         
-            if (typeOfImages == 2) {
+            if (typeOfImages == 2 && arPIDs.length > 0) {
                 var includePIDs = prompt('Theres PIDs on this page, want to include those?\n1. Yes\n2. No');
                 if (includePIDs == 1) {
                     userDefinedPIDs = prompt('Type multiple 6 digit PIDs below to search them on the ' + site + ' site.\n(Dont worry about removing spaces)',arPIDs.join(','));
