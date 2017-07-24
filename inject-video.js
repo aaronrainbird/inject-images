43aec9a1352dc51abdbaac6f23124530a53305d9
aaronrainbird <aaronrainbird@gmail.com>
2017-07-24 10:48:10 +0100

    Update inject-video.js

--- inject-video.js
+++ inject-video.js
@@ -43,57 +43,136 @@ var mrpImageHeight = 475;
 var mrpVideoWidth = 464;
 var mrpVideoHeight = 475;
 
-var tableHTML = "<html><head><link href='http://vjs.zencdn.net/5.8.8/video-js.css' rel='stylesheet'><script src='http://vjs.zencdn.net/5.8.8/video.js'></script><!-- If you'd like to support IE8 --><script src='http://vjs.zencdn.net/ie8/1.1.2/videojs-ie8.min.js'></script><script language = 'javascript'>function updateMedia() { for (var t = 0;t<document.getElementsByTagName('video').length;t++) { if (document.getElementsByTagName('source')[t].src.search('t=03') != -1) {document.getElementsByTagName('source')[t].src = document.getElementsByTagName('source')[t].src.substr(0,document.getElementsByTagName('source')[t].src.search('mp4')+3) + '?' + escape(new Date()) + '#t=03'; } else { document.getElementsByTagName('source')[t].src = document.getElementsByTagName('source')[t].src.substr(0,document.getElementsByTagName('source')[t].src.search('mp4')+3) + '?' + escape(new Date()); } document.getElementsByTagName('video')[t].load(); document.getElementsByTagName('video')[t].pause(); } for (var x = 0;x<document.getElementsByTagName('img').length;x++) { document.getElementsByTagName('img')[x].src = document.getElementsByTagName('img')[x].src.substr(0,document.getElementsByTagName('img')[x].src.search('jpg')+3); } }</script><!-- If you'd like to support IE8 --><script src='http://vjs.zencdn.net/ie8/1.1.2/videojs-ie8.min.js'></script></head><body><input type='button' value='Click to refresh images and videos on page' onclick='updateMedia()'><table class='tg' style='text-align: center; font-family: Georgia; border: 1px solid black;'><tr><th class='header'>OU</th><th class='header'>Video</th><th class='header'>IN</th></tr><tr>"
+var tableHTML = "<html><head><link href='http://vjs.zencdn.net/5.8.8/video-js.css' rel='stylesheet'><script src='http://vjs.zencdn.net/5.8.8/video.js'></script><!-- If you'd like to support IE8 --><script src='http://vjs.zencdn.net/ie8/1.1.2/videojs-ie8.min.js'></script></head><body><table class='tg' style='text-align: center; font-family: Georgia; border: 1px solid black;'><tr><th class='header'>OU</th><th class='header'>Video</th><th class='header'>IN</th></tr><tr>"
 
 
 defineChoices();
 
- var timeDelay = escape(new Date());   
-    var timeChoice = prompt("Start from beginning or a few seconds in for colour matching? Choose 1 or 2. \n1. Start from beginning \n2. Few seconds in");
-    if (timeChoice == 2) {
-        timeDelay += "#t=03";
-    }
-
-    if (subSite == "fulcrum") {   //Fulcrum
-        for (var i = 0; i < arPIDs.length; i++) {
-            for (var a = 0; a < 3; a++) {
-               tableCell();
-            }
-                  tableHTML += "</tr><tr><td colspan='3' style='border: 1px solid black;'>"  + arFulcrumProductDescriptionPIDs[i] + "</SPAN><BR><SPAN>Colour: " + arFulcrumColourPIDs[i] + "</SPAN></td></tr>"; 
-            
-        }
-    }
-
-    else {        // Get Pid No's from page
-        for (var i = 0; i < arPIDs.length; i++) {
-
-            for (var a = 0; a < arImageTypes.length;a++) {
-                tableCell();
-            }
-                        
-                tableHTML += "</tr>";    // Put more info in here later if appropriate.
-            
-        }
-
-    }
-
-tableHTML += "</table>";
-
+function defineChoices() {                      //  Identifies what subsite you're on and what the image classes are on the live site.
+  
     
-var x = window.open();
-x.document.open();
-x.document.write(tableHTML);
-x.document.close();
+    
+    if(document.getElementsByClassName('channel_logo')[0] != undefined) {
+         subSite = "fulcrum";
+         getFulcrumInfo();
+    
+        if(document.getElementsByClassName('channel_logo')[0].alt.search('MRP') != -1)  {
+              site = "MrP";      
+        }
+        else if(document.getElementsByClassName('channel_logo')[0].alt.search('NAP') != -1) {
+              site = "NAP";   
+        }
+    }
+    else if (document.URL.search('net-a-porter') != -1) {
+        site = "NAP";
+        imageClass = "product-image";
+    }
+    else if (document.URL.search('mrporter') != -1) {
+        site = "MrP";
+        if (document.getElementsByClassName('pl-products-item__link').length > 1) {
+            imageClass = "pl-products-item__link";
+        }
+        else {
+            imageClass = "product-image";
+        }    
+    }
+   
+    findPid();    // Finds the PIDs on the page.
+             
+    if (arPIDs.length > 0) {
+        typeOfImages = prompt('What images would you like to view on the ' + site + ' site? \n1. All images on Page\n2. User defined PIDs');
+    }
+    else {
+        if (subSite != "fulcrum") {
+            if (site != "NAP" && site != "MrP") {
+                var siteChoice = prompt("You don't appear to be on any of the YNAP group sites. Which site would you like to search? Choose 1 or 2. \n1. NAP \n2. MrP");
+                if (siteChoice == 1) {
+                    site = "NAP";
+                }
+                else {
+                    site = "MrP";
+                }
+          
+              userDefinedPIDs = prompt('Type multiple 6 or 7 digit PIDs below to search them on the ' + site + ' site.\n \n Any separator can be used (space,comma,hyphen) just dont mix them. \n \n E.g:652651,521521,6353434 (comma separated) not:251252, 424142, 242424 (which has a comma AND a space) \n \n');
+              userDefinedPIDsSeparator = userDefinedPIDs.match(/\D/)[0]
+              arPids = userDefinedPIDs.split(userDefinedPIDsSeparator)
+              // userDefinedPIDs = userDefinedPIDs.replace(/[^0-9]+/g, '');   
+              // arPIDs = userDefinedPIDs.match(/.{1,6}/g)
+
+      
+            }
+          }
+      }
+        
+    if (typeOfImages == 2 && arPIDs.length > 0) {     // Chosen user defined PIDs but there's some on the page.
+        var includePIDs = prompt('There are PIDs on this page, want to include those?\n1. Yes\n2. No');
+        if (includePIDs == 1) {
+               userDefinedPIDs = prompt('Type multiple 6 digit PIDs below to search them on the ' + site + ' site.\n(Dont worry about removing spaces)',arPIDs.join(","));
+              userDefinedPIDs = userDefinedPIDs.replace(/[^0-9]+/g, '');
+              arPIDs = userDefinedPIDs.match(/.{1,6}/g)
+        }
+        else {
+               userDefinedPIDs = prompt('Type multiple 6 digit PIDs below to search them on the ' + site + ' site.\n(Dont worry about removing spaces)');
+              userDefinedPIDs = userDefinedPIDs.replace(/[^0-9]+/g, '');
+              arPIDs = userDefinedPIDs.match(/.{1,6}/g)
+        }
+    }
+    else if (arPIDs.length == 0) {    // No PID numbers on the page, just take user search.
+         userDefinedPIDs = prompt('Type multiple 6 digit PIDs below to search them on the ' + site + ' site.\n(Dont worry about removing spaces)');
+              userDefinedPIDs = userDefinedPIDs.replace(/[^0-9]+/g, '');
+              arPIDs = userDefinedPIDs.match(/.{1,6}/g)
+    }
+    
+ }
 
 
+function findPid() {                // Finds the PIDs on the page on NAP or MrP sites only.  NOT FULCRUM.
+   
+    for (var j = 0; j < document.getElementsByClassName(imageClass).length; j++) {
+         
+        if (site == "MrP") {
+            
+            if (document.getElementsByClassName('pl-products-item__link').length > 1) {
+                var pidsPath = document.getElementsByClassName(imageClass)[j].href; 
+            }
+            else if (document.getElementsByClassName('product-carousel__image  product-carousel__item--current') != undefined) {
+              console.log("test");
+                var pidsPath = document.getElementsByClassName(imageClass)[j].childNodes[1].childNodes[1].src;
+                
+            }
 
+          if (pidsPath != undefined) {
+          
+            var pidsFull = pidsPath.replace(/^.*[\\\/]/, '');         
+            arPIDs.push(pidsFull.slice(0,6));
+          }
+           
+        }
+         
+        if (site == "NAP") {
 
+            var pidsPath = document.getElementsByClassName(imageClass)[j].childNodes[1].pathname;
+
+            if (pidsPath.slice(0,8) == '/product') {
+                var pidsFull = pidsPath.replace('/product/', '');    
+
+            }
+            else {
+                var pathArray = window.location.pathname.split( '/' );
+                var pidsFull = pidsPath.replace("/" + pathArray[1] + "/" + pathArray[2] + "/product/", '');
+            }
+
+            arPIDs.push(pidsFull.slice(0,6));
+
+        }
+        
+    }
+    
+}
 
 
 function tableCell() {
-   
-    
-    
+     
     if (a == 1) {   // 2nd cell is video cell.
          if (site == "MrP") {
              
@@ -138,51 +217,8 @@ function tableCell() {
         }
 }
 
-function findPid() {
-   
-    for (var j = 0; j < document.getElementsByClassName(imageClass).length; j++) {
-         
-        if (site == "MrP") {
-            
-            if (document.getElementsByClassName('pl-products-item__link').length > 1) {
-                var pidsPath = document.getElementsByClassName(imageClass)[j].href; 
-            }
-            else if (document.getElementsByClassName('product-carousel__image  product-carousel__item--current') != undefined) {
-              console.log("test");
-                var pidsPath = document.getElementsByClassName(imageClass)[j].childNodes[1].childNodes[1].src;
-                
-            }
 
-          if (pidsPath != undefined) {
-          
-            var pidsFull = pidsPath.replace(/^.*[\\\/]/, '');         
-            arPIDs.push(pidsFull.slice(0,6));
-          }
-           
-        }
-         
-        if (site == "NAP") {
-
-            var pidsPath = document.getElementsByClassName(imageClass)[j].childNodes[1].pathname;
-
-            if (pidsPath.slice(0,8) == '/product') {
-                var pidsFull = pidsPath.replace('/product/', '');    
-
-            }
-            else {
-                var pathArray = window.location.pathname.split( '/' );
-                var pidsFull = pidsPath.replace("/" + pathArray[1] + "/" + pathArray[2] + "/product/", '');
-            }
-
-            arPIDs.push(pidsFull.slice(0,6));
-
-        }
-        
-    }
-    
-}
-
-function imageFileLocation() {
+function imageFileLocation() {                      //  Pulls the location of the files.
     
    if (site == "MrP") {  //MrP
         filename = arPIDs[i] + "_mrp_" + arImageTypes[a];
@@ -198,84 +234,9 @@ function imageFileLocation() {
 }
 
 
-function defineChoices() {
-  
-    
-    
-    if(document.getElementsByClassName('channel_logo')[0] != undefined) {
-         subSite = "fulcrum";
-         getFulcrumInfo();
-    
-        if(document.getElementsByClassName('channel_logo')[0].alt.search('MRP') != -1)  {
-              site = "MrP";      
-        }
-        else if(document.getElementsByClassName('channel_logo')[0].alt.search('NAP') != -1) {
-              site = "NAP";   
-        }
-    }
-    else if (document.URL.search('net-a-porter') != -1) {
-        site = "NAP";
-        imageClass = "product-image";
-    }
-    else if (document.URL.search('mrporter') != -1) {
-        site = "MrP";
-        if (document.getElementsByClassName('pl-products-item__link').length > 1) {
-            imageClass = "pl-products-item__link";
-        }
-        else {
-            imageClass = "product-image";
-        }    
-    }
-   
-    findPid();
-             
-    if (arPIDs.length > 0) {
-        typeOfImages = prompt('What images would you like to view on the ' + site + ' site? \n1. All images on Page\n2. User defined PIDs');
-    }
-    else {
-        if (subSite != "fulcrum") {
-            if (site != "NAP" && site != "MrP") {
-                var siteChoice = prompt("You don't appear to be on any of the YNAP group sites. Which site would you like to search? Choose 1 or 2. \n1. NAP \n2. MrP");
-                if (siteChoice == 1) {
-                    site = "NAP";
-                }
-                else {
-                    site = "MrP";
-                }
-          
-              userDefinedPIDs = prompt('Type multiple 6 digit PIDs below to search them on the ' + site + ' site.\n(Dont worry about removing spaces)');
-                userDefinedPIDs = userDefinedPIDs.replace(/mp4/g,'');  
-              userDefinedPIDs = userDefinedPIDs.replace(/[^0-9]+/g, '');
-              arPIDs = userDefinedPIDs.match(/.{1,6}/g)
 
-      
-            }
-          }
-      }
-        
-    if (typeOfImages == 2 && arPIDs.length > 0) {
-        var includePIDs = prompt('Theres PIDs on this page, want to include those?\n1. Yes\n2. No');
-        if (includePIDs == 1) {
-               userDefinedPIDs = prompt('Type multiple 6 digit PIDs below to search them on the ' + site + ' site.\n(Dont worry about removing spaces)',arPIDs.join(","));
-            userDefinedPIDs = userDefinedPIDs.replace(/mp4/g,'');      
-          userDefinedPIDs = userDefinedPIDs.replace(/[^0-9]+/g, '');
-              arPIDs = userDefinedPIDs.match(/.{1,6}/g)
-        }
-        else {
-               userDefinedPIDs = prompt('Type multiple 6 digit PIDs below to search them on the ' + site + ' site.\n(Dont worry about removing spaces)');
-            userDefinedPIDs = userDefinedPIDs.replace(/mp4/g,'');      
-          userDefinedPIDs = userDefinedPIDs.replace(/[^0-9]+/g, '');
-              arPIDs = userDefinedPIDs.match(/.{1,6}/g)
-        }
-    }
-    else if (arPIDs.length == 0) {
-         userDefinedPIDs = prompt('Type multiple 6 digit PIDs below to search them on the ' + site + ' site.\n(Dont worry about removing spaces)');
-      userDefinedPIDs = userDefinedPIDs.replace(/mp4/g,'');       
-      userDefinedPIDs = userDefinedPIDs.replace(/[^0-9]+/g, '');
-              arPIDs = userDefinedPIDs.match(/.{1,6}/g)
-    }
-    
- }
+
+
 
 function getFulcrumInfo() {
     
@@ -283,7 +244,7 @@ function getFulcrumInfo() {
           
     for (var l=0; l < document.getElementById('photography_worklist_details').childNodes[0].childNodes[2].childNodes.length; l++) {
     
-            if (document.getElementById('photography_worklist_details').childNodes[0].childNodes[2].childNodes[l].childNodes.length > 19) {
+            if (document.getElementById('photography_worklist_details').childNodes[0].childNodes[2].childNodes[l].childNodes.length > 19) {    //Confirm it's a row?
                
                                    
                     if (document.getElementById("image_icon_" +  document.getElementById('photography_worklist_details').childNodes[0].childNodes[2].childNodes[l].childNodes[1].childNodes[0].childNodes[1].childNodes[0].childNodes[1].textContent + "_15").src.search("film") > 0) {
@@ -319,6 +280,50 @@ function getFulcrumInfo() {
     typeOfImages = 5;    
     }
 }
+
+
+
+
+// Ask when to start in video.
+
+var timeDelay = escape(new Date());   
+var timeChoice = prompt("Start from beginning or a few seconds in for colour matching? Choose 1 or 2. \n1. Start from beginning \n2. Few seconds in");
+if (timeChoice == 2) {
+    timeDelay += "#t=03";
+}
+
+if (subSite == "fulcrum") {   //Fulcrum
+    for (var i = 0; i < arPIDs.length; i++) {
+        for (var a = 0; a < 3; a++) {
+            tableCell();
+        }
+                tableHTML += "</tr><tr><td colspan='3' style='border: 1px solid black;'>"  + arFulcrumProductDescriptionPIDs[i] + "</SPAN><BR><SPAN>Colour: " + arFulcrumColourPIDs[i] + "</SPAN></td></tr>"; 
+        
+    }
+}
+
+else {        // Get Pid No's from page
+    for (var i = 0; i < arPIDs.length; i++) {
+
+        for (var a = 0; a < arImageTypes.length;a++) {
+            tableCell();
+        }
+                    
+            tableHTML += "</tr>";    // Put more info in here later if appropriate.
+        
+    }
+
+}
+
+tableHTML += "</table>";
+
+// Write table to new tab.
+
+var x = window.open();
+x.document.open();
+x.document.write(tableHTML);
+x.document.close();
+
 
 
 function clean(node)
