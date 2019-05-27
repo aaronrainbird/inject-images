c909c154bcac918882a6fabcb642e670f8055d2c
aaronrainbird <aaronrainbird@gmail.com>
2019-05-27 12:53:05 +0100

    Update cellBGandBlendingMode.js

--- cellBGandBlendingMode.js
+++ cellBGandBlendingMode.js
@@ -4,11 +4,12 @@ if (imageDiv.length == 0) {
 imageDiv = document.getElementsByClassName('thumbnail')
 }
 
-var greyRGB = prompt("Please insert your grey RGB value i.e 240","240")
+var RGB = prompt("Please insert your RGB values:","240,240,240")
 
 var blendingMode = prompt("Please type a blending mode out of one of the following: \n \n normal|multiply|screen|overlay|darken|lighten|color-dodge|color-burn|difference|exclusion|hue|saturation|color|luminosity;","multiply")
 
 for (var a = 0;a<imageDiv.length;a++) {
-imageDiv[a].style.backgroundColor = "rgb(" + greyRGB + "," + greyRGB + "," + greyRGB + ")";
+imageDiv[a].style.backgroundColor = "rgb(" + RGB + ")";
+imageDiv[a].style.padding = "20px 20px 20px 20px";
 imageDiv[a].children[0].children[0].style = "mix-blend-mode: " + blendingMode;
 }
